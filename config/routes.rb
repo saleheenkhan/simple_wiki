f12a3ed046c5fc830a5cdc0b53893532812f9443
nynhex <nynhex@gmail.com>
2017-10-07 09:29:33 -0500

    Scope out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: 'registrations'}
+  resources :articles
+  resources :user_admin do
+    member do
+      put 'disable'
+    end
+  end
+  resources :categories, only: [:index, :show, :create]
+  resources :tags, only: [:index, :show, :create]
+  get :search, controller: :main
+  root to: 'articles#index'
 end
