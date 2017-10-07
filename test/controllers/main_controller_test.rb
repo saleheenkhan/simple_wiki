1459b07f74e2327481aa4881bfbbb3b253d4c785
nynhex <nynhex@gmail.com>
2017-10-07 10:22:01 -0500

    Fuck testing

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class ArticlesControllerTest < ActionDispatch::IntegrationTest
+class MainControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get articles_index_url
+    get main_index_url
     assert_response :success
   end
 
